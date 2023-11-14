82e7112f8fc461f28c279ee2830bf9d7a16da54a
rqhappyy <dteresenko203@gmail.com>
2023-11-14 20:39:13 +0300

    task4

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
-    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+    public static void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 
     }
 }
